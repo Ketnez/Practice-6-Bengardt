274c7c3967e62a94ddd80b6cdd0ec434f4989348
ИСП-34 <-34@LAB24-09>
2021-12-07 16:54:07 +0300

    Добавьте файлы проекта.

---
+++ Practice-6-Bengardt/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice_6_Bengardt
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            Pair firstP = new Pair(int.Parse(FIRSTP1.Text), int.Parse(SECONDP1.Text));
+            Pair secondP = new Pair(int.Parse(FIRSTP2.Text), int.Parse(SECONDP2.Text));
+            Pair resultP = firstP.Substract(secondP);
+            result.Text = (resultP.First, resultP.Second).ToString();
+        }
+
+        private void ComparisonPair_Click(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(int.Parse(FIRSTP1.Text), int.Parse(SECONDP1.Text));
+            result.Text = pair ? "true" : "false";
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Practice-6-Bengardt/Pair.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_6_Bengardt
+{
+    internal class Pair
+    {
+        public int First { get; private set; }
+        public int Second { get; private set; }
+
+        public Pair(int first, int second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public static bool operator true(Pair pair)
+        {
+            return pair.First == pair.Second;
+        }
+
+        public static bool operator false(Pair pair)
+        {
+            return pair.First == pair.Second;
+        }
+
+        public Pair Substract(Pair subtrahend)
+        {
+            return new Pair(First - subtrahend.First, Second - subtrahend.Second);
+        }
+    }
+}
